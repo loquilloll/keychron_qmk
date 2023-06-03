42b34e609f81cf7f13bb529312556f17fbd2c683
Alvin Sanchez <loquilloll@gmail.com>
2023-06-03 15:48:43 -0400

    rebuild

--- keymap.c
+++ keymap.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [WIN_BASE] = LAYOUT_65_ansi(
         KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_6, KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,   KC_EQL,    KC_BSPC,          RGB_MOD,
-        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_Y, KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,   KC_BSLASH, KC_NO,            KC_HOME,
+        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_Y, KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,   KC_BSLASH, _______,          KC_HOME,
         LCTL_KESC, LGUI_KA, LALT_KS, LCTL_KD, LSFT_KF, KC_G, KC_H, RSFT_KJ, RCTL_KK, RALT_KL, RG_SCLN, KC_QUOT,              KC_ENT,           KC_PGUP,
         KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_N, KC_M,    KC_COMM, KC_DOT,  KC_SLSH,            KC_RSFT,          KC_UP,     KC_PGDOWN, 
         KC_LCTL, KC_LGUI, KC_LALT,                   LT1_SPC,                        KC_RCTRL, MO(WIN_FN1), MO(FN2),     KC_LEFT,   KC_DOWN,   KC_RGHT
